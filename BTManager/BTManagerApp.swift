9fae69ad222193a25a91fbaf1f76a0a1692ebbb3
lenny_cheng <langrisser1981@gmail.com>
2023-01-07 21:22:31 +0800

    Initial Commit

---
+++ BTManager/BTManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTManagerApp.swift
+//  BTManager
+//
+//  Created by 程信傑 on 2023/1/7.
+//
+
+import SwiftUI
+
+@main
+struct BTManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BTManager
+//
+//  Created by 程信傑 on 2023/1/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
